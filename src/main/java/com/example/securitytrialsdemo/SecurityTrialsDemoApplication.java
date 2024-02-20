5e04132e00f871f8c8997c77e6a8cca6b0630714
Abdallah-20 <https://github.com/Abdallah-20>
2024-02-20 15:37:06 +0200

    first commit

---
+++ src/main/java/com/example/securitytrialsdemo/SecurityTrialsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitytrialsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTrialsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTrialsDemoApplication.class, args);
+	}
+
+}
